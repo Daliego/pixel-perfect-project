1395eff25aff3102b0e891809230c72c7205025c
Daliego <daliego82@gmail.com>
2025-03-30 19:22:49 -0300

    fix/headind-without-check-action

--- src/components/home/TodoList.tsx
+++ src/components/home/TodoList.tsx
@@ -4,26 +4,54 @@ import { Separator } from "../@shared/Separator";
 import { listData } from "../../mockData/listData";
 import { ListItem } from "./ListItem";
 import { Button } from "../@shared/Button";
+import { Item } from "../../models/item";
 
 const buttonContainer: React.CSSProperties = {
-  // Talvez fixar os 60px
   padding: "10px 15px 10px 15px",
 };
 
 const listBodyContainer: React.CSSProperties = {
-  // maxHeight: "200px",
   overflowY: "scroll",
   scrollbarWidth: "none",
 };
 
 export function TodoList() {
+  // I didnt look onto other implementation for the heading,
+  // because i didnt think it was necessary for a small project
+  const [heading, setHeading] = useState<Item>({
+    id: "0",
+    isChecked: false,
+    text: "All pages",
+  });
+
   const [list, setList] = useState(listData);
+
+  const handleChange = (id: string, list: Item[]) => {
+    const selectedItem = list.find((i) => i.id === id)!;
+
+    selectedItem.isChecked = !selectedItem.isChecked;
+
+    setList((oldList) =>
+      oldList.map((i) =>
+        i.id === selectedItem.id
+          ? { ...i, isChecked: selectedItem.isChecked }
+          : i
+      )
+    );
+  };
 
   return (
     // Based on the figma component the proportion screen component would
     // be 64%, but i let the size of the figma todo list.
     <Card style={{ width: "370px", height: "326px" }}>
-      <ListItem title="All pages" checked={false} onClick={() => {}} id={"0"} />
+      <ListItem
+        title={heading.text}
+        checked={heading.isChecked}
+        onClick={() =>
+          setHeading((old) => ({ ...old, isChecked: !old.isChecked }))
+        }
+        id={heading.id}
+      />
       <Separator />
       <div style={listBodyContainer}>
         {list.map((item) => (
@@ -32,24 +60,7 @@ export function TodoList() {
             key={item.id}
             title={item.text}
             checked={item.isChecked}
-            onClick={() => {
-              const selectedItem = list.find((i) => i.id === item.id)!;
-
-              selectedItem.isChecked = !selectedItem.isChecked;
-
-              setList((oldList) =>
-                oldList.map((i) =>
-                  i.id === selectedItem.id
-                    ? { ...i, isChecked: selectedItem.isChecked }
-                    : i
-                )
-              );
-              console.log(item);
-              // const newList = list.map((i) =>
-              //   i.id === item.id ? { ...i, isChecked: !i.isChecked } : i
-              // );
-              // setList(newList);
-            }}
+            onClick={() => handleChange(item.id, list)}
           />
         ))}
       </div>
