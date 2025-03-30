c9fc5b1e1646042b1b8ee87f882d3277bb780206
Daliego <daliego82@gmail.com>
2025-03-30 18:52:41 -0300

    hotfix/errors-in-the-repo-name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "pixel-perfect-project",
   plugins: [react()],
+  base: "/pixel-perfect-project/",
 });
