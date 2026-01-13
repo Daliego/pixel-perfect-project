3029ca6b47ece965567301e83dfc27b828867183
Daliego <daliego82@gmail.com>
2026-01-13 11:37:25 -0300

    refactor: update Separator component styles for improved layout and appearance

--- src/components/@shared/Separator.tsx
+++ src/components/@shared/Separator.tsx
@@ -1,14 +1,17 @@
-import { colors } from "../../layouts/colors";
-
 const style = {
-  line: {
-    backgroundColor: colors.border.gray,
-    height: "0px",
-    borderBottom: `.7px solid ${colors.border.gray}`,
+  container: {
+    width: "370px",
+    height: "20px",
+    padding: "10px 15px",
+    display: "flex",
+    alignItems: "center",
+    boxSizing: "border-box",
   } as React.CSSProperties,
 
-  container: {
-    padding: "10px 15px 10px 15px",
+  line: {
+    width: "340px",
+    height: "0px",
+    borderBottom: "0.7px solid #CDCDCD",
   } as React.CSSProperties,
 };
 
